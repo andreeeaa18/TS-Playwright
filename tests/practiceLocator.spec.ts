436f7592a700810b1dc1a032f0950db70aa6a682
andreeeaa18 <andreevandreea.18@gmail.com>
2025-06-23 16:22:08 +0300

    CreatedPractice with getBy Methods

--- tests/practiceLocator.spec.ts
+++ tests/practiceLocator.spec.ts
@@ -1,4 +1,5 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
+import { TIMEOUT } from "dns";
 
 test("Practice of Locator Method", async ({ page }) => {
   await page.goto("https://www.saucedemo.com");
@@ -30,4 +31,34 @@ test("Practice of Locator Method with Options", async ({ page }) => {
   await page.locator("#login-button").click();
   // await page.locator("//a", { hasText: "Sauce Labs Backpack" }).click();
   await page.locator(".inventory_item_name", { hasNotText: /Sauce.*/ }).click();
+});
+
+test("Practice with getBy Methods", async ({ page }) => {
+  await page.goto("https://demo.nopcommerce.com/login");
+  await page.getByLabel("Email").fill("testcodeautomaye@gmail.com");
+  await page.getByPlaceholder("Search store").fill("Mobile");
+
+  await expect(page.getByText("New Customer")).toBeVisible();
+  const newCustomerText = await page
+    .getByText("New Customer", { exact: true })
+    .textContent();
+  console.log("New Customer text:", newCustomerText);
+
+  await page.getByAltText("nopCommerce demo store").click();
+
+  // await page.locator("#PKVDd5").waitFor({ timeout: 2000 });
+  // await page.locator("#PKVDd5").click();
+  // await page.getByAltText("nopCommerce demo store").click();
+  // await page
+  //   .getByTitle("Show products in category Electronics")
+  //   .first()
+  //   .click();
+});
+
+test("Practice with getBy Methods #2", async ({ page }) => {
+  // await page.goto("https://demo.nopcommerce.com/electronics");
+  // await page.getByRole("button", { name: "Search" }).click();
+
+  await page.goto("https://www.saucedemo.com");
+  await page.getByTestId("username").fill("TestCodeAutomate");
 });
