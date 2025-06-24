c177736f9796eb696ffdbcc5adf4c9b835ed0cd0
andreeeaa18 <andreevandreea.18@gmail.com>
2025-06-24 23:27:08 +0300

    Created differentClickOps.spec.ts

---
+++ tests/differentClickOps.spec.ts
@@ -0,0 +1,24 @@
+import { test } from "@playwright/test";
+
+test("Diff click ops", async ({ page }) => {
+  //   await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  //   await page.getByRole("button", { name: "Add Element" }).dblclick();
+
+  await page.goto("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+  await page
+    .getByText("right click me", { exact: true })
+    .click({ button: "right" });
+  await page.getByRole("listitem").filter({ hasText: "Quit" }).click();
+
+  await page
+    .getByRole("heading", { name: "Example code: Simple Context" })
+    .click({ button: "right" });
+
+  //   await page.goto("https://staging.engineer.ai/home");
+  //   await page.getByRole("button", { name: "Choose a base" }).click();
+  //   await page.getByText("Actually I'll browse the").click();
+  //   await page.waitForTimeout(3000);
+  //   await page
+  //     .getByRole("button", { name: "o Custom Template" })
+  //     .dispatchEvent("click");
+});
