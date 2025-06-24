6b4a92f41198aeb975962c1dca3a0ba1ea0a8d71
andreeeaa18 <andreevandreea.18@gmail.com>
2025-06-24 11:09:57 +0300

    Made some changes in the config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: "always" }]],
+  reporter: [["html", { open: "always", outputFolder: "./customFolder" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,7 +33,7 @@ export default defineConfig({
     testIdAttribute: "data-test",
     screenshot: "on",
     video: "retain-on-failure",
-    headless: false,
+    // headless: false,
   },
 
   /* Configure projects for major browsers */
